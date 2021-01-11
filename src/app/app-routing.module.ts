aea211e039e350e2bc84de3cc2188118fd7bf1e4
Amir Azer <aazer@itps.com>
2021-01-11 11:44:21 +0200

    FaceId

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path: 'home',
@@ -8,8 +9,12 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
 ];
 
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  @Input() isModal: boolean;
+  constructor(private faio: FingerprintAIO, private router: Router, private modalCtrl: ModalController) { }
+
+  ngOnInit() {
+    console.log('i am modal:', this.isModal);
+  }
+
+  login(){
+    this.faio.show({
+      
+
+    }).then (() =>{
+      if (this.isModal) {
+        this.modalCtrl.dismiss();
+      } else {
+        
+      this.router.navigateByUrl('/home');
+      }
+
+    })
+    .catch ((error : any) => {
+      console.log('err:' , error);
+    });
+    
+
+  }
+
+}
